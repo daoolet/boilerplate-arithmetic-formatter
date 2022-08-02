2d49e9095718c569e89006d4cfcc4a65dad62698
Daulet <dauletkhabidullin@gmail.com>
2022-08-02 20:05:51 +0500

    Solved

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,9 @@
 def arithmetic_arranger(problems):
-
-
-    return arranged_problems
+    ans = []
+    for el in problems:
+        for i in el.split(','):
+            if i.split()[1] == '+':
+                ans.append(int(i.split()[0]) + int(i.split()[-1]))
+            if i.split()[1] == '-':
+                ans.append(int(i.split()[0]) - int(i.split()[-1]))
+    return ans
